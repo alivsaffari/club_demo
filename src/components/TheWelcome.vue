ff35f4e86effe1776fdac9401af04efa0a2f49aa
Ali Saffari <alivsaffari@gmail.com>
2024-08-03 16:12:33 +0330

    tonconnect lib added

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
 const club_name = defineModel('club_name')
+const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
+  manifestUrl: 'https://club-demo.vercel.app/tonconnect-manifest.json',
+  buttonRootId: 'ton-connect'
+})
+tonConnectUI.uiOptions = {
+  twaReturnUrl: 'https://t.me/token_cafe_bot/token_app'
+}
+
+async function connectToWallet() {
+  const connectedWallet = await tonConnectUI.connectWallet()
+  // Do something with connectedWallet if needed
+  console.log(connectedWallet)
+}
+
+function call_wallet() {
+  // Call the function
+  connectToWallet().catch((error) => {
+    console.error('Error connecting to wallet:', error)
+  })
+}
 </script>
 
 <template>
-  <form class="establish_form" action="">
+  <form class="establish_form" @submit="call_wallet">
     <h1>🏆</h1>
     <h1>Establish Your Club!</h1>
     <input
@@ -13,7 +33,7 @@ const club_name = defineModel('club_name')
       v-model="club_name"
       placeholder="Club Name"
     />
-    <button class="btn btn-primary">Let's Start!</button>
+    <button type="submit" class="btn btn-primary" id="ton-connect">Let's Start!</button>
   </form>
 </template>
 
